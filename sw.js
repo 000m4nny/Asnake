1cb122ff859730f4507df6ff2b15063c606877a8
000m4nny <amanuel.asnake@icloud.com>
2025-01-07 18:19:20 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,26 @@
 const CACHE_NAME = "planner-cache-v1";
 const urlsToCache = [
-    "/homework-planner/",
-    "/homework-planner/index.html",
-    "/homework-planner/style.css",
-    "/homework-planner/app.js",
-    "/homework-planner/manifest.json",
-    "/homework-planner/icons/icon-192x192.png",
-    "/homework-planner/icons/icon-512x512.png"
-  ];  
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
